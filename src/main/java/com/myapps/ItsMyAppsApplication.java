8447a0f913cbada6ec15656ca279c9e8a30e1231
Ramanatha <ramsiit2006@gmail.com>
2025-04-26 22:46:47 +0530

    code

---
+++ src/main/java/com/myapps/ItsMyAppsApplication.java
@@ -0,0 +1,20 @@
+package com.myapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ItsMyAppsApplication {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Hi, Welcome to My Apps World";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(ItsMyAppsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapps/ItsMyAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsMyAppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
